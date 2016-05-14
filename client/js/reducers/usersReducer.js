66a633744b735742a94b9a15aef2802b0ea2aa4e
Jędrzej Kuryło <jedrzej.kurylo@gmail.com>
2016-05-14 13:02:55 +0200

    Fixes #155 sort teams and users alphabetically
    Fixes #152 make avatars round in profile page
    
    Fixes #151 disable  when no team/user is selected
    
    Added ramda helper library, refactored teams and profiles pages

--- client/js/reducers/usersReducer.js
+++ client/js/reducers/usersReducer.js
@@ -13,6 +13,8 @@ import {
 } from '../constants/AppConstants';
 
 import assignToEmpty from '../utils/assign';
+import { sortTeamsByName } from '../utils/common';
+
 
 // Export initial state to avoid repetition on tests.
 export const initialState = {};
@@ -25,11 +27,13 @@ function usersReducer(users = initialState, action) {
     case USER_LIST_SUCCESS:
       newUsers = {};
       action.users.forEach((user) => {
+        processUser(user);
         newUsers[user.id] = user;
       });
       return newUsers;
     case USER_GET_SUCCESS:
     case USER_UPDATE_SUCCESS:
+      processUser(action.user);
       return assignToEmpty(users, {
         [action.user.id]: action.user
       });
@@ -61,6 +65,10 @@ function usersReducer(users = initialState, action) {
     default:
       return users;
   }
+}
+
+function processUser(user) {
+  user.teams.sort(sortTeamsByName);
 }
 
 export default usersReducer;
